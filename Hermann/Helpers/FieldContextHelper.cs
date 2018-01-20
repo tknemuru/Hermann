7577b8f9468b4b0e40eecacd418e6e16b1b5404b
tknemuru <kojikoji.max.5252@gmail.com>
2018-01-21 02:53:48 +0900

    helper mod

--- Hermann/Helpers/FieldContextHelper.cs
+++ Hermann/Helpers/FieldContextHelper.cs
@@ -41,42 +41,5 @@ namespace Hermann.Helpers
         {
             return ((context.SlimeFields[(int)player][slime][index] & (1u << position)) > 0u);
         }
-
-        /// <summary>
-        /// 移動可能なスライムを初期位置に配置します。
-        /// </summary>
-        /// <param name="context">フィールドの状態</param>
-        /// <param name="player">プレイヤ</param>
-        /// <param name="slimes">セットするスライム</param>
-        /// <param name="movables">セット対象の移動可能スライム</param>
-        public static void SetMovableSlimeInitialPosition(FieldContext context, Player.Index player, Slime[] slimes, MovableSlime[] movables)
-        {
-            Debug.Assert(slimes.Length == MovableSlime.Length, string.Format("スライムの数が不正です。数：{0}", slimes.Length));
-            Debug.Assert(movables.Length == MovableSlime.Length, string.Format("移動可能なスライムの数が不正です。数：{0}", movables.Length));
-
-            MovableSlime.ForEach((unitIndex) =>
-            {
-                movables[(int)unitIndex].Slime = slimes[(int)unitIndex];
-                movables[(int)unitIndex].Index = FieldContextConfig.HiddenUnitIndex;
-                movables[(int)unitIndex].Position = FieldContextConfig.MovableSlimeInitialShift + ((int)unitIndex * FieldContextConfig.OneLineBitCount);
-                context.MovableSlimes[(int)player][(int)unitIndex] = movables[(int)unitIndex];
-
-                // フィールドにも反映させる
-                context.SlimeFields[(int)player][movables[(int)unitIndex].Slime][movables[(int)unitIndex].Index] |= 1u << movables[(int)unitIndex].Position;
-            });
-        }
-
-        /// <summary>
-        /// 移動可能なスライムを初期位置に配置します。
-        /// </summary>
-        /// <param name="context">フィールドの状態</param>
-        /// <param name="player">プレイヤ</param>
-        /// <param name="slimes">セットするスライム</param>
-        public static void SetMovableSlimeInitialPosition(FieldContext context, Player.Index player, Slime[] slimes)
-        {
-            var movables = new MovableSlime[MovableSlime.Length];
-            MovableSlime.ForEach((i) => movables[(int)i] = new MovableSlime());
-            SetMovableSlimeInitialPosition(context, player, slimes, movables);
-        }
     }
 }
